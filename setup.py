3dbfd0de3a7f8a7c68208cd066c11cb8f0c99c39
taikob <taisukekbys@gmail.com>
2022-10-17 17:46:06 +0900

    first comit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="make_stimuli",
+    version="1.0",
+    author="Taisuke Kobayashi",
+    author_email="taisukekbys@gmail.com",
+    description="make_stimuli is my own python package",
+    long_description="make_stimuli",
+    long_description_content_type="text/markdown",
+    url="https://github.com/taikob",
+    packages=setuptools.find_packages()
+)
